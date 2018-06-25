2f01bd666f970faac186e5290b4a7535db5024be
Boyu Jiang <jbyjiangboyu@126.com>
2018-06-25 14:21:58 +0800

    Test run

--- motion-correct/parameter.py
+++ motion-correct/parameter.py
@@ -11,7 +11,7 @@ results = []
 face_to_list_dict = dict()
 list_to_face_dict = dict()
 
-group_similarity_src = 'group5.csv'
+group_similarity_src = 'data/group5.csv'
 
 cnn_build_time_range_set = [['2018-01-03 15:30:00', '2018-01-03 16:59:59'], ['2018-01-18 15:30:00', '2018-01-18 15:59:59']]
 predict_time_range_set = [['2018-01-09 15:30:00', '2018-01-09 15:59:59'], ['2018-01-11 15:30:00', '2018-01-11 15:59:59'],
